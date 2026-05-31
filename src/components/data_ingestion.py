321342375589a7134922875487777d88706469e2
mehul-aws20 <mehulkumar10dsmhs@gmail.com>
2026-05-31 10:59:01 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import pandas as pd
+
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+from src.exception import CustomException
+from src.logger import logging
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+
+        logging.info("Entered Data Ingestion Method")
+
+        try:
+            print("Reading dataset...")
+
+            df = pd.read_csv(
+                os.path.join("notebook", "data", "stud.csv")
+            )
+
+            print("Dataset loaded successfully")
+            logging.info("Dataset loaded as DataFrame")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path),
+                exist_ok=True
+            )
+
+            print("Artifacts folder created")
+
+            df.to_csv(
+                self.ingestion_config.raw_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Train-Test split initiated")
+
+            train_set, test_set = train_test_split(
+                df,
+                test_size=0.2,
+                random_state=42
+            )
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index=False,
+                header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Data ingestion completed")
+
+            print("Train.csv created")
+            print("Test.csv created")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+
+    obj = DataIngestion()
+
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    print("\nFiles Generated:")
+    print(train_path)
+    print(test_path)
